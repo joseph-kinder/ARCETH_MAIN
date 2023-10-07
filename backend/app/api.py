bd3b1a329d63234e4005eb04f252c4552e7cd735
crypanon <j.kinder@lse.ac.uk>
2023-10-07 15:30:55 +0100

    Fix secret

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,7 +1,10 @@
 import requests
+import os
+
+bearer_token = os.environ.get("BEARER_TOKEN")
 
 API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": f"Bearer {bearer_token}"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
